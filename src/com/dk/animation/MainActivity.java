59ee9422e119e9f255f675dbabcbeafb0466358b
Peter Ding <peterding@augmentum.com.cn>
2014-08-28 17:45:28 +0800

    init commit

---
+++ src/com/dk/animation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dk.animation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
