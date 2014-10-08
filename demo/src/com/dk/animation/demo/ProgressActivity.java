c67ad8f8683ace37ea103933f3401520baed66c7
Peter Ding <peterding@augmentum.com.cn>
2014-10-08 18:08:52 +0800

    refine progress demo

--- demo/src/com/dk/animation/demo/ProgressActivity.java
+++ demo/src/com/dk/animation/demo/ProgressActivity.java
@@ -22,151 +22,146 @@ import com.dk.animation.effect.out.DropOut;
 
 public class ProgressActivity extends Activity {
 
-	private RecyclerView mRecyclerView;
-	private ListAdapterHolder adapter;
-	private ArrayList<UserData> mDatas = new ArrayList<UserData>();
-	private static int[] mResId = new int[] { R.drawable.p1, R.drawable.p2,
-			R.drawable.p3 };
-	private BaseItemAnimator mAnimator;
-	private View mRefreshIcon, mProgress;
-	private Badge mBadge;
-	private int count = 0;
+    private RecyclerView mRecyclerView;
+    private ListAdapterHolder mAdapter;
+    private ArrayList<UserData> mDatas = new ArrayList<UserData>();
+    private static int[] mResId = new int[] { R.drawable.p1, R.drawable.p2, R.drawable.p3 };
+    private BaseItemAnimator mAnimator;
+    private View mRefreshIcon, mProgress;
+    private Badge mBadge;
+    private int count = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_progress);
-		getActionBar().hide();
-		ViewUtils.init(this);
-		mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-		mBadge = (Badge) findViewById(R.id.badge);
-		createTestData();
-		adapter = new ListAdapterHolder(mDatas);
-		mRecyclerView.setAdapter(adapter);
-		mRecyclerView.setHasFixedSize(true);
-		mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-		mAnimator = new PackageAnimator(new AnchorDropIn(
-				findViewById(R.id.refresh)), new AnchorDropOut(
-				findViewById(R.id.trash)));
-		mAnimator.setAddDuration(800);
-		mRecyclerView.setItemAnimator(mAnimator);
-		adapter.setOnItemClickListener(new OnItemClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+        getActionBar().hide();
+        ViewUtils.init(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mBadge = (Badge) findViewById(R.id.badge);
+        createTestData();
+        mAdapter = new ListAdapterHolder(mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAnimator = new PackageAnimator(new AnchorDropIn(findViewById(R.id.refresh)), new AnchorDropOut(findViewById(R.id.trash)));
+        mAnimator.setAddDuration(800);
+        mRecyclerView.setItemAnimator(mAnimator);
+        mAdapter.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(View view, int position) {
-				mAnimator = new PackageAnimator(null, new AnchorDropOut(
-						findViewById(R.id.trash)));
-				mAnimator.setAddDuration(800);
-				mRecyclerView.setItemAnimator(mAnimator);
+            @Override
+            public void onItemClick(View view, int position) {
+                mAnimator = new PackageAnimator(null, new AnchorDropOut(findViewById(R.id.trash)));
+                mAnimator.setAddDuration(800);
+                mRecyclerView.setItemAnimator(mAnimator);
 
-				removeItem(position);
-				count++;
-				mBadge.setText(String.valueOf(count));
-			}
-		});
-		mRefreshIcon = findViewById(R.id.refresh);
-		mProgress = findViewById(R.id.progressBar);
-		mRefreshIcon.setOnClickListener(mOnClickListener);
-		findViewById(R.id.trash).setOnClickListener(new View.OnClickListener() {
+                removeItem(position);
+                count++;
+                mBadge.setText(String.valueOf(count));
+            }
+        });
+        mRefreshIcon = findViewById(R.id.refresh);
+        mProgress = findViewById(R.id.progressBar);
+        mRefreshIcon.setOnClickListener(mOnClickListener);
+        findViewById(R.id.trash).setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-				count = 0;
-				mBadge.setText("");
+                count = 0;
+                mBadge.setText("");
 
-				mAnimator = new PackageAnimator(new AnchorDropIn(
-						findViewById(R.id.trash)), new AnchorDropOut(
-						findViewById(R.id.trash)));
-				mAnimator.setAddDuration(400);
-				mRecyclerView.setItemAnimator(mAnimator);
+                mAnimator = new PackageAnimator(new AnchorDropIn(findViewById(R.id.trash)), new AnchorDropOut(findViewById(R.id.trash)));
+                mAnimator.setAddDuration(400);
+                mRecyclerView.setItemAnimator(mAnimator);
 
-				for (int i = 0; i < 3; i++) {
-					addItem(0);
-				}
-				mRecyclerView.postDelayed(new Runnable() {
+                for (int i = 0; i < 3; i++) {
+                    addItem(0);
+                }
+                mRecyclerView.postDelayed(new Runnable() {
 
-					@Override
-					public void run() {
+                    @Override
+                    public void run() {
 
-						adapter.notifyItemRangeInserted(0, 3);
-						mRecyclerView.scrollToPosition(0);
+                        mAdapter.notifyItemRangeInserted(0, 3);
+                        mRecyclerView.scrollToPosition(0);
+                        ViewUtils.disableScroll(mRecyclerView, 800 + 800 + 200 * 3);
 
-					}
-				}, 1);
-			}
-		});
-	}
+                    }
+                }, 1);
+            }
+        });
+    }
 
-	private View.OnClickListener mOnClickListener = new OnClickListener() {
+    private View.OnClickListener mOnClickListener = new OnClickListener() {
 
-		@Override
-		public void onClick(View v) {
-			mRefreshIcon.setVisibility(View.GONE);
-			mProgress.setVisibility(View.VISIBLE);
-			TestTask task = new TestTask();
-			task.execute("test");
-		}
-	};
+        @Override
+        public void onClick(View v) {
+            mRefreshIcon.setVisibility(View.GONE);
+            mProgress.setVisibility(View.VISIBLE);
+            TestTask task = new TestTask();
+            task.execute("test");
+        }
+    };
 
-	private void addItems() {
-		mAnimator = new PackageAnimator(new AnchorDropIn(
-				findViewById(R.id.refresh)), new AnchorDropOut(
-				findViewById(R.id.trash)));
-		mAnimator.setAddDuration(800);
-		mRecyclerView.setItemAnimator(mAnimator);
+    private void addItems() {
+        mAnimator = new PackageAnimator(new AnchorDropIn(findViewById(R.id.refresh)), new AnchorDropOut(findViewById(R.id.trash)));
+        mAnimator.setAddDuration(800);
+        mRecyclerView.setItemAnimator(mAnimator);
 
-		for (int i = 0; i < 3; i++) {
-			addItem(0);
-		}
-		mRecyclerView.postDelayed(new Runnable() {
+        for (int i = 0; i < 3; i++) {
+            addItem(0);
+        }
+        mRecyclerView.postDelayed(new Runnable() {
 
-			@Override
-			public void run() {
+            @Override
+            public void run() {
 
-				adapter.notifyItemRangeInserted(0, 3);
-				mRecyclerView.scrollToPosition(0);
+                mAdapter.notifyItemRangeInserted(0, 3);
+                mRecyclerView.scrollToPosition(0);
 
-			}
-		}, 1);
+            }
+        }, 1);
 
-	}
+    }
 
-	private void addItem(int position) {
-		mDatas.add(position, new UserData(mResId[position % 3], "Hello World"));
+    private void addItem(int position) {
+        mDatas.add(position, new UserData(mResId[position % 3], "Hello World"));
 
-	}
+    }
 
-	private void removeItem(int position) {
-		mDatas.remove(position);
-		adapter.notifyItemRemoved(position);
-	}
+    private void removeItem(int position) {
+        mDatas.remove(position);
+        mAdapter.notifyItemRemoved(position);
+        mAdapter.disableEventForAniamtion(800 + 800);
+        ViewUtils.disableScroll(mRecyclerView, 800 + 800);
+    }
 
-	private void createTestData() {
-		for (int i = 0; i < 20; i++) {
-			mDatas.add(new UserData(mResId[i % 3], "Hello World"));
-		}
-	}
+    private void createTestData() {
+        for (int i = 0; i < 20; i++) {
+            mDatas.add(new UserData(mResId[i % 3], "Hello World"));
+        }
+    }
 
-	private class TestTask extends AsyncTask<String, Void, String> {
+    private class TestTask extends AsyncTask<String, Void, String> {
 
-		@Override
-		protected String doInBackground(String... params) {
-			// do sth
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
+        @Override
+        protected String doInBackground(String... params) {
+            // do sth
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-		@Override
-		protected void onPostExecute(String result) {
-			addItems();
+        @Override
+        protected void onPostExecute(String result) {
+            addItems();
 
-			mRefreshIcon.setVisibility(View.VISIBLE);
-			mProgress.setVisibility(View.GONE);
-		}
-	};
+            mRefreshIcon.setVisibility(View.VISIBLE);
+            mProgress.setVisibility(View.GONE);
+        }
+    };
 }
